ec8e676983f57b66ef9e6661b1fc6cfb7c475332
Dror Bogin <Dror.Bogin@gmail.com>
2018-05-27 16:54:46 +0300

    Update legend.js

--- js/legend.js
+++ js/legend.js
@@ -1,7 +1,9 @@
 function open_legend(){
 				
-				var legendstring = "<div><span><h3>Beer Sheva Time Variation</h3><button id='legendbut2' type='button' title='Close legend' onclick='close_legend()'>&#9746</button></span>"+
-					"<p>The map shows time variation between real-time bus location data and bus schedules." +
+				var legendstring = "<div><span><h3>Check how many Cellular Towers are in an area</h3><button id='legendbut2' type='button' title='Close legend' onclick='close_legend()'>&#9746</button></span>"+
+					"<p>The map allows you to check how many cellular antenas are within 500 meters from every point you click"+
+					" all you have to do is click on the buttn to add the buffers and click on the map to check a selected location.<br>"+
+					"It can also show time variation between real-time bus location data and bus schedules if you turn on the layer." +
 					"real-time data was collected over a week in July 2017 the data was collected using the <b>R</b> programming language, </p>" +
 					"<p>with HTTP POST querying Israel's Ministery Of Transportation's server to query the server and recieve" +
 					"a response containing the <a href='http://user47094.vs.easily.co.uk/siri/documentation.html'>SIRI</a> data transfer protocol." +
@@ -17,9 +19,6 @@ function open_legend(){
 					"<p>As you may have noticed both the Legend is expandable and the</br>" +
 					"Layer+Tile list are collapsable, the data layer can be turned off</br>" +
 					"and more tile layers are available for viewing. </p>" +
-					"<p><b>*The update for the second project</b> are the buttons to the left, "+
-					"they add a partial GeoJSON on top of the data layer to show areas where most of the buses are late or early. "+
-					"A third button removes the partial GeoJSON</p>"+ 
 					"<p><center>______________________________________________________________________</center></p>" +
 					"<b>Mean Time Variation by color: </b><br><br>" +
 					"<span><mark style='background-color: rgba(179, 0, 0,1); color: rgba(179, 0, 0,1);'>XX</mark><span><b>Late</b> (More than 1 Minute)</span></span>" +
@@ -37,7 +36,7 @@ function open_legend(){
 									'border-radius': '5px',
 									'position': 'fixed',
 									'left': '30%',
-									'top': '12%',
+									'top': '5%',
 									'width': '600px',
 									'display': 'block'});
 				$("#container").css('width', '600px');
